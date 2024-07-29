4e077fd9770585ba2838d529430faabf57c2aef2
anup soni <anupsoni2526@gmail.com>
2024-07-29 18:42:34 +0530

    [initial Commit]

---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <Container fluid className="footer text-center">
+      <Row>
+        <Col className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/Anup-Soni2511"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="display-6"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/anup-soni-a61a271ab/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="display-6"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/soumyajit4419"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="display-6"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/data/workExperienceData.js
@@ -0,0 +1,27 @@
+import codetradeLogo from "../Assets/CompanyLogo/codetrade-logo.svg";
+
+const workExperienceData = [
+    {
+        position: 'Software Developer',
+        company: 'Codetrade India Pvt. Ltd.',
+        startDate: 'January 2023',
+        endDate: 'Present',
+        responsibilities: [
+          'Developing and maintaining web applications.',
+
+          'Collaborated with a team of developers to design and implement robust and \
+           scalable Python-based solutions, web applications \
+           using Django Rest Framework and ReactJS.',
+          
+          'Utilized version control systems, primarily Git, for efficient \
+          collaboration and code management.',
+          
+          'Participated in code reviews to ensure code quality,\
+           performance, and adherence to coding standards.',
+        ],
+        website: 'https://www.codetrade.io/',
+        logo: codetradeLogo
+      }
+    ];
+
+export default workExperienceData;
